567e90445d3289642f2dee1de0ac36f5a815b0e9
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-18 22:32:46 +0300

    add Random Example003 Summ  project

--- Example003_Summ/Program.cs
+++ Example003_Summ/Program.cs
@@ -1,4 +1,7 @@
-﻿// Написать программу для сложения двух чисел
-int numberA = 3;
-int numberB = 5;
+﻿// Написать программу для сложения 
+// двух случайных чисел отт 1 до 10
+int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA+numberB);
