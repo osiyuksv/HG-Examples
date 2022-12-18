d8d3782b07cae80cabfc0fdbddf98cff6e677734
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-18 23:15:19 +0300

    add Example 005 Condition IfElse project

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// Как приветствовать пользователя-любимчика Маша
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+  Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.WriteLine(username);
+}
