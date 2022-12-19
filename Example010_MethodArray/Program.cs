544795d85a968413cc77a1bbb6a0da31db90b2e9
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-19 12:42:32 +0300

    add Example 010 Method Array simple project

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,19 @@
+﻿// в массиве из n эдементов найти 
+// индекс первого элемента равный заданному find
+int[] array = {1, 12, 31, 4, 15, 16, 17, 18};
+
+int n = array.Length;
+int find = 4;
+int index = 0;
+
+while(index < n)
+{
+  if(array[index] == find)
+  {
+    Console.Write("Номер заданного первого элемента в массиве = ");
+    Console.WriteLine(index);
+    break;
+  }
+    index++;
+}
+
