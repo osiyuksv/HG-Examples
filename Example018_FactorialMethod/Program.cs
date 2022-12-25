91ccda964cad028713649b7ce8847af6ca018f54
Sergey Osiyuk <osiyuksv@mail.ru>
2022-12-25 11:56:50 +0300

    Example018_FactorialMethod

---
+++ Example018_FactorialMethod/Program.cs
@@ -0,0 +1,15 @@
+﻿// метод, который принимает число N
+// и вычисляет его факториал
+
+double Factorial(int N)
+{
+  if(N == 1) return(1);
+  else return N * Factorial(N - 1);
+}
+Console.WriteLine(Factorial(5));
+Console.WriteLine();
+
+for (int i = 1; i < 40; i++)
+{
+  Console.WriteLine($"{i}! = {Factorial(i)}");
+}
